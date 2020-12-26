799356a1cade8053d4929928b020942a56f0f2a4
alexmaryin <java73@yandex.ru>
2020-12-26 12:07:43 +0400

    Parametrized group test was added with xfail catch.

--- test_product_page.py
+++ test_product_page.py
@@ -1,13 +1,31 @@
-import time
+import pytest
+
 from pages.product_page import ProductPage
 
-link_shellcoders = r'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
+link_shell_coders = r'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
 link_coders_at_work = r'http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/?promo=newYear2019'
 
 
-def test_guest_can_add_product_to_basket(browser):
+def test_guest_can_add_product_to_basket_ny(browser):
     product_page = ProductPage(browser, link_coders_at_work)
     product_page.open()
     product_page.add_to_basket_promo('newYear')
     product_page.is_product_in_basket_popups()
 
+
+@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+                                  pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
+                                               marks=pytest.mark.xfail),
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+def test_guest_can_add_product_to_basket(browser, link):
+    product_page = ProductPage(browser, link)
+    product_page.open()
+    product_page.add_to_basket_promo(link.partition('promo=')[2])
+    product_page.is_product_in_basket_popups()
