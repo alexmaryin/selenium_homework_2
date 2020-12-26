0cae3f7afeb6519c1e291008755311edadb9b07d
alexmaryin <java73@yandex.ru>
2020-12-26 09:46:54 +0400

    Explicit going to login/registration page

--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,3 @@
-from .pages.login_page import LoginPage
 from .pages.main_page import MainPage
 
 
@@ -7,6 +6,5 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
-    # page.go_to_login_page()
-    # login_page = LoginPage(browser, 'http://selenium1py.pythonanywhere.com/ru/accounts/login/')
-    # login_page.should_be_login_page()
+    login_page = page.go_to_login_page()
+    login_page.should_be_login_page()
